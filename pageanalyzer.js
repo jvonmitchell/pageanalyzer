c40acc4f4d428f25f1901dfdac46629cabef54de
Jason Mitchell <jvonmitchell@gmail.com>
2013-07-05 17:50:58 -0400

    Initial pageanalyzer.js added

---
+++ pageanalyzer.js
@@ -0,0 +1,43 @@
+var a = function () {
+ var crypto = require('crypto'),
+  htmlparser = require('htmlparser2'),
+  spawn = require('child_process').spawn;
+  
+ var loadedurls = {};
+ 
+ GLOBAL.getItemAttributes = function(url, attributes, callback) {
+  var parser = new htmlparser.Parser( {onopentag : function(name, attribs) {
+   attribs.tagName = name;
+   for( key in attributes ) {
+    if( attributes.hasOwnProperty(key) ) {
+     if( attributes[key] !== attribs[key] ) {
+      return;
+     }
+    }
+   }
+   callback(attribs);
+  }}),
+   hash = crypto.createHash('md5');
+  hash.update(url);
+  hash = hash.digest('hex');
+  if( loadedurls[hash] ) {
+   if( loadedurls[hash].join ) {
+    loadedurls[hash] = loadedurls[hash].join('');
+   }
+   parser.write(loadedurls[hash]);
+   parser.end();
+  }
+  else {
+   loadedurls[hash] = [];
+   var curl = spawn('curl', [url]);
+   curl.stdout.on('data', function(data) {
+    loadedurls[hash].push(data);
+    parser.write(data);
+   });
+   curl.on('exit', function(code) {
+    parser.end();
+   });
+  }
+ }
+}
+a();
